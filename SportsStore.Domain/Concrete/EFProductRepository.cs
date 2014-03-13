c4d11ade7b7a94247be3413b88faafc7c8af2561
ChernetsIvan <chernets.ivan@gmail.com>
2014-03-13 12:34:14 +0300

    first initial commit

---
+++ SportsStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,31 @@
+﻿//using System.Linq;
+//using SportsStore.Domain.Abstract;
+//using SportsStore.Domain.Entities;
+
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using System.Data;
+
+
+
+
+
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private EFDbContext context = new EFDbContext();
+        public IQueryable<Product> Products
+        {
+            get { return context.Products; }
+        }
+    }
+}
---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        
+        // позволит Ninject внедрять зависимость хранилища товаров при созда­нии экземпляра класса контроллера
+        public ProductController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
